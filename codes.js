cb695804d6b4877323a02321122781c0d927019c
JUNAisKIND <0627juna@naver.com>
2022-03-04 01:25:12 +0900

    완성

---
+++ codes.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector(".navbar_toggle");
+
+const nav = document.querySelector(".navbar");
+const menu = document.querySelector(".navbar_menu");
+const sns = document.querySelector(".navbar_sns");
+
+toggleBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  menu.classList.toggle('active');
+  sns.classList.toggle('active');
+})
+class afterText extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({mode : 'open'});
+    this.shadowRoot.innerHTML = `<span></span><style>span::after {content: "${this.innerHTML}";}</style>`
+  }
+}
+customElements.define('af-t', afterText);
+
+function changeImg(id, img) { //id format : type1, type2, type3 ...
+  const docu = document.querySelector(`#${id} div img`);
+  docu.setAttribute("src", `images/${id}/${img}.jpg`)
+}
